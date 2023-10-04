8b12d86428972e716d5a8bc763bfef466ec59976
Adolfo Mendoza <adolfitocraftero1225@gmail.com>
2023-10-03 19:05:22 -0600

    03/10/2023 AAMP

--- src/app/calculos/cinepolis-form/cinepolis-form.component.ts
+++ src/app/calculos/cinepolis-form/cinepolis-form.component.ts
@@ -7,22 +7,45 @@ import { Component } from '@angular/core';
 })
 export class CinepolisFormComponent 
 {
-      nombre:string = "";
-      catComprador:number = 0;
-      catBoletos:number = 0;
-      valor:number = 0;
-      total:number = 0;
+  nombre: string = "";
+  catComprador: number = 0;
+  catBoletos: number = 0;
+  valor: number = 0;
+  total: number = 0;
+  tarjeta: string = "";
 
-      pagar():void
-      {
-           if (this.catBoletos > 5) 
-           {
-               
-           }
-
-           else 
-           {
-            
-           }
-      }
+  Pagar(): void {
+    const catBoletos = this.catBoletos;
+    const catComprador = this.catComprador;
+    const tarjeta = this.tarjeta === 'Si';
+  
+    let descuento = 0;
+  
+    if (catBoletos >= 5) 
+    {
+      descuento = 0.15; 
+    } 
+    
+    else if (catBoletos >= 3 && catBoletos <= 5) 
+    {
+      descuento = 0.10; 
+    }
+  
+    const precioBoleto = 12.000;
+  
+    let valor = catBoletos * precioBoleto * catComprador; 
+    if (descuento > 0) 
+    {
+      valor -= valor * descuento;
+    }
+  
+    if (tarjeta) 
+    {
+      const descuentoTarjeta = 0.10; 
+      valor -= valor * descuentoTarjeta;
+    }
+  
+    this.valor = valor;
+    this.total = valor;
+  }
 }
